d049238e2714d5681af63ac5224d8fa6df86a72f
Florian Dewes <dewes@htw-berlin.de>
2025-03-11 15:42:18 +0100

    Dont log cron activities

--- bergwerk-api/app/service/tracker.py
+++ bergwerk-api/app/service/tracker.py
@@ -3,5 +3,6 @@ from data import db as data_db
 
 def track_text(uid, role, text, buttons):
     ts = datetime.now().timestamp()
-    data_db.track_text(uid, role, text, buttons, ts)
+    if uid != "cron":
+        data_db.track_text(uid, role, text, buttons, ts)
     
