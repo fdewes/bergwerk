bad8c99ebdfdea6097611ca8f55cb0f19bf194b8
Florian Dewes <dewes@htw-berlin.de>
2025-03-12 12:54:35 +0100

    Add users per day plot to stats page

--- bergwerk-cron/app/stats.py
+++ bergwerk-cron/app/stats.py
@@ -12,12 +12,29 @@ page = """
 This page is updated every day with the latest statistics for this assistant.
 
 == All Time Users == 
-
-
-[[File:test.jpg|thumb]]
+[[File:upd.png|thumb]]
 """
 
-df = tools.sql2pd(tracker_db, "tracker")
+df = tools.sql2pd("tracker_db", "tracker")
+
+page += "<markdown>\n" + df.head().to_markdown() + "</markdown>\n"
 
 
-tools.create_or_update_page("Stats", page, ["/usr/src/app/test.jpg"])
+df['datetime'] = pd.to_datetime(df['ts'], unit='s')
+df['year'] =  df['datetime'].dt.year
+df['month'] =  df['datetime'].dt.month
+df['day'] =  df['datetime'].dt.day
+df['dow'] = df['datetime'].dt.dayofweek
+df['hour'] =  df['datetime'].dt.hour
+df['date'] = df['datetime'].dt.date
+unique_users_per_day = df.groupby('date')['id'].nunique()
+
+
+plt.figure(figsize=(12, 6))
+plt.plot(unique_users_per_day.index, unique_users_per_day.values)
+plt.title('Number of Unique Users per Day')
+plt.xlabel('Date')
+plt.ylabel('Number of unique users')
+plt.savefig("/usr/src/app/upd.png")
+
+tools.create_or_update_page("Stats", page, ["/usr/src/app/upd.png"])
