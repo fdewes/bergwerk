4e93613fb4a295a087aa20b38092f5442f0e5977
Florian Dewes <dewes@htw-berlin.de>
2025-03-12 13:21:42 +0100

    Add users per week plot

--- bergwerk-cron/app/stats.py
+++ bergwerk-cron/app/stats.py
@@ -12,13 +12,16 @@ page = """
 This page is updated every day with the latest statistics for this assistant.
 
 == All Time Users == 
-[[File:upd.png|thumb]]
+
+[[File:upd_all_time.png|thumb]]
+
+== Users per Week ==
+
+[[File:upw_all_time.png|thumb]]
+
 """
 
 df = tools.sql2pd("tracker_db", "tracker")
-
-page += "<markdown>\n" + df.head().to_markdown() + "</markdown>\n"
-
 
 df['datetime'] = pd.to_datetime(df['ts'], unit='s')
 df['year'] =  df['datetime'].dt.year
@@ -35,6 +38,18 @@ plt.plot(unique_users_per_day.index, unique_users_per_day.values)
 plt.title('Number of Unique Users per Day')
 plt.xlabel('Date')
 plt.ylabel('Number of unique users')
-plt.savefig("/usr/src/app/upd.png")
+plt.savefig("/usr/src/app/upd_all_time.png")
 
-tools.create_or_update_page("Stats", page, ["/usr/src/app/upd.png"])
+df['year_week'] = df['datetime'].dt.strftime('%Y-%U')
+unique_users_per_week = df.groupby('year_week')['id'].nunique()
+unique_users_per_week = unique_users_per_week.sort_index()
+
+# Plot the result
+unique_users_per_week.plot(kind='bar', figsize=(12, 6), title='Number of Unique Users per Week')
+plt.xlabel('Year and Week')
+plt.ylabel('Number of Unique Users')
+plt.xticks(rotation=45)
+plt.savefig("/usr/src/app/upw_all_time.png")
+
+
+tools.create_or_update_page("Stats", page, ["/usr/src/app/upd_all_time.png", "/usr/src/app/upw_all_time.png"])
